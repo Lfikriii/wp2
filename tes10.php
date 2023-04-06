0b9cee415e7d26bdcdce8bbb22b9a566e7046f76
Lfikri <80593937+Lfikriii@users.noreply.github.com>
2023-04-06 12:03:32 +0700

    belajar include (modularitas)

---
+++ tes10.php
@@ -0,0 +1,14 @@
+<?php
+include "tes9.php";
+
+$jumlah = $a + $b;
+$kurang = $a - $b;
+$bagi = $a / $b;
+$kali = $a * $b;
+$mod = $a % $b;
+
+echo "hasil dari " . $a . " + " . $b . " =  " . $jumlah . "<br>";
+echo "hasil dari " . $a . " - " . $b . " = " . $kurang . "<br>";
+echo "hasil dari " . $a . " / " . $b . " = " . $bagi . "<br>";
+echo "hasil dari " . $a . " * " . $b . " = " . $kali . "<br>";
+echo "hasil dari " . $a . " % " . $b . " = " . $mod;
