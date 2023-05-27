66a0e3cc452e9ddf817571dbebe33f65415030b4
Lfikri <80593937+Lfikriii@users.noreply.github.com>
2023-05-27 23:46:11 +0700

    persiapan bootsrap 5 untuk uas

---
+++ UAS/CRUD/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+    <h1 class="text-center">Hello, world!</h1>
+
+
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
